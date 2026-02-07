cc829059b63f66f71bfc9441270a81a0eafd6550
Michael <100490812+DebSol11@users.noreply.github.com>
2026-02-07 17:38:25 +0400

    Understanding react better

---
+++ src/components/cvLetter.jsx
@@ -0,0 +1,7 @@
+import Intro from "./components/introduction";
+
+export default function renderIntro() {
+    return (
+        <h1>{name}</h1>
+    )
+}
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,16 +1,59 @@
 import { useState } from "react";
+import "../styles/style.css";
 
 export default function Intro() {
-    return (
-        <>
-            <div>
-                <label htmlFor="name">Name: </label>
-                <input type="text" placeholder="Please, enter your name:"/>
-            </div>
-            <div>
-                <label htmlFor="purpose">Purpose of writing: </label>
-                <input type="text" placeholder="Please, enter the purpose for writing this letter:"/>
-            </div>
-        </>
-    )
+  let [name, setName] = useState("");
+  // let [purpose, setPurpose] = useState("");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  // function handlePurposeChange(e) {
+  //   setPurpose(e.target.value);
+  // }
+
+  return (
+    <div className="intro">
+      <IntroInput name={name} handleNameChange={handleNameChange} />
+    </div>
+  );
 }
+
+function IntroInput({ name, handleNameChange }) {
+  return (
+    <div className="intro-input">
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          placeholder="Please, enter your name:"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </div>
+      {/* <div>
+        <label htmlFor="purpose">Purpose of writing: </label>
+        <input
+          type="text"
+          placeholder="Please, enter the purpose for writing this letter:"
+          value={purpose}
+          onChange={() => handlePurposeChange}
+        />
+      </div> */}
+      <div className="intro-render">
+        <h1>{name}</h1>
+        {/* <h2>{purpose}</h2> */}
+      </div>
+    </div>
+  );
+}
+
+// function IntroRender({ name, purpose }) {
+//   return (
+//     <div className="intro-render">
+//       <h1>{name}</h1>
+//       <h2>{purpose}</h2>
+//     </div>
+//   );
+// }
